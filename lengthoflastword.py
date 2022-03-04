c451d40ae5e3a3cdeb20267032b0998e168f5c0f
Ron <100487379+NixeFR@users.noreply.github.com>
2022-03-04 17:44:58 +0100

    Update lengthoflastword.py

--- lengthoflastword.py
+++ lengthoflastword.py
@@ -1,3 +1,4 @@
+# Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
 def lengthOfLastWord(s):
     x = s.rstrip(' ')
     list(x)
